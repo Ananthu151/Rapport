d491a4f5cb4a5ea6e2fab3ef106800faa9c8d650
Ananthu Suresh <93853939+Ananthu151@users.noreply.github.com>
2023-01-15 21:07:24 +0530

    Add files via upload

---
+++ lib/widgets/dailyupdate.dart
@@ -0,0 +1,11 @@
+List<String> dailyUpdate = [
+  "Today marks the start of your menses.Health care professionals date your pregnancy from your Last Menstrual Period (LMP), so make sure you note down the first day of your last menstruation.",
+  "Week 1 No,Updation!",
+  "Week 2 No,Updation!",
+  "Since conception and fertilization don’t happen yet at this week, your baby is technically just an idea - but a beautiful one at that!",
+  "Week 4 No,Updation!",
+  "Week 5 No,Updation!",
+  "Though you’re not technically pregnant yet, all the ingredients are ready for baking! You will soon ovulate, and with the right timing, the sperm from your partner will fertilize your egg.",
+  "Ovulation happens this week! To increase your chances of becoming pregnant, have sex at least two days before and two days after your ovulation date.",
+  "",
+];
